77ef97a027bfff039a88dab023899107d1939de2
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2021-09-19 23:13:54 -0300

    Final aula 2 - Leitura de dados json e urlencoded

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const consign = require('consign')
+const consign = require('consign');
 
 module.exports = () => {
   const app = express();
+
+  //Não é mais necessário utilizar a biblioteca body-parser. Utilize o próprio express
+  app.use(express.json()); //o servidor consegue ler dados em json que foram enviados para a API. Isso depende do Content-Type (application/json)
+  app.use(express.urlencoded({ extended: true })); //servidor consegue ler dados urlencoded que foram enviados para a API. (application/x-www-form-urlencoded)
 
   consign() //chama o consign
     .include('controllers') //inclui todos os módulos da pasta de controllers
