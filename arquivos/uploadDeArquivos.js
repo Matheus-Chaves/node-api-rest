6e40fe935b1347d993b50f65c7d87e2c19c7b5e5
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2021-10-12 21:09:53 -0300

    curso 2, final aula 2 -manipulando arquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,7 +1,8 @@
 const fs = require('fs'); //file system
+const path = require('path')
 
 // fs.readFile("./assets/bob.jpg", (erro, buffer) => {  //função SÍNCRONA. Lê um arquivo e gera um buffer dele
-//   console.log("Imagem foi bufferizada")
+//   console.log("Imagem foi 'bufferizada' ")
 //   console.log(buffer)
 
 //   fs.writeFile("./assets/bob2.jpg", buffer, (erro) => { //Escreve (ou cria e escreve) em um arquivo o que for passado como parâmetro no 2° item
@@ -9,6 +10,19 @@ const fs = require('fs'); //file system
 //   })
 // })
 
-fs.createReadStream("./assets/bob.jpg") //Função assíncrona, cria uma Stream de leitura da imagem
-  .pipe(fs.createWriteStream('./assets/bob-stream.jpg')) //Faz com que a Stream de leitura seja transformada em uma stream de escrita
-  .on('finish', () => console.log("Imagem foi escrita com sucesso")) //'on' é ativado quando a condição ('finish') é atingida. Nesse caso, o callback acontece assim que o pipe é finalizado
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => { //exporta uma função que precisa de 3 parâmetros: caminho do arquivo; nome do arquivo; função de callback
+  const tiposValidos = ['jpg', 'jpeg', 'png']
+  const tipo = path.extname(caminho) //Pega a extensão (tipo) do arquivo (.png, .txt, .jpg etc.)
+  const tipoEhValido = (tiposValidos.indexOf(tipo.substring(1)) !== -1) //Se -1 é pq não é um tipo válido. Verifica se o tipo é válido, usa substring(1) para remover o ponto da extensão (.png vira png)
+  const novoCaminho = `./assets/images/${nomeDoArquivo}${tipo}`
+
+  if (tipoEhValido) {
+    fs.createReadStream(caminho) //Função assíncrona, cria uma Stream de leitura da imagem
+      .pipe(fs.createWriteStream(novoCaminho)) //Faz com que a Stream de leitura seja transformada em uma stream de escrita
+      .on('finish', () => callbackImagemCriada(false, novoCaminho)) //'on' é ativado quando a condição ('finish') é atingida. Nesse caso, o callback acontece assim que o pipe é finalizado
+  } else {
+    const erro = "O tipo da imagem é inválido!"
+    console.log("Erro! O tipo da imagem é inválido!")
+    callbackImagemCriada(erro)
+  }
+}
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,7 +1,9 @@
 class Tabelas {
   init(conexao) { //init() é utilizado para inicializar a classe assim que ela for chamada
     this.conexao = conexao; //Variável conexão recebe o parâmetro enviado no index.js
+
     this.criarAtendimentos(); //Invoca a função
+    this.criarPets();
   }
 
   criarAtendimentos() {
@@ -21,6 +23,21 @@ class Tabelas {
     this.conexao.query(sql, err => {
       err ? console.error(err) : console.log('Tabela Atendimentos criada com sucesso.')
     }) //o método 'query' recebe vários parâmetros, sendo o primeiro um código SQL e o último um código de erro
+  }
+
+  criarPets() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS Pets (
+        id int NOT NULL AUTO_INCREMENT,
+        nome varchar(50),
+        imagem varchar(200),
+        PRIMARY KEY(id)
+      )
+    `
+
+    this.conexao.query(sql, err => {
+      err ? console.log(err) : console.log('Tabela Pets criada com sucesso!')
+    })
   }
 }
 
